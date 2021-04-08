cff8a8b93d76c00edcb72dd91c8939c3e8724b66
David Ziegler <david-ziegler@posteo.de>
2021-04-08 23:25:39 +0200

    new / change path for deployment-script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
-scp index.ts ziedav@delphinus.uberspace.de:~/telegramListBot/
+scp index.ts ziedav@delphinus.uberspace.de:~/listBot/
 
-scp -r src ziedav@delphinus.uberspace.de:~/telegramListBot/
-scp -r scripts ziedav@delphinus.uberspace.de:~/telegramListBot/
+scp -r src ziedav@delphinus.uberspace.de:~/listBot/
+scp -r scripts ziedav@delphinus.uberspace.de:~/listBot/
 
-scp package.json ziedav@delphinus.uberspace.de:~/telegramListBot/
-scp yarn.lock ziedav@delphinus.uberspace.de:~/telegramListBot/
+scp package.json ziedav@delphinus.uberspace.de:~/listBot/
+scp yarn.lock ziedav@delphinus.uberspace.de:~/listBot/
 
-scp tsconfig.json ziedav@delphinus.uberspace.de:~/telegramListBot/
-scp .eslintrc.json ziedav@delphinus.uberspace.de:~/telegramListBot/
-scp .eslintignore ziedav@delphinus.uberspace.de:~/telegramListBot/
+scp tsconfig.json ziedav@delphinus.uberspace.de:~/listBot/
+scp .eslintrc.json ziedav@delphinus.uberspace.de:~/listBot/
+scp .eslintignore ziedav@delphinus.uberspace.de:~/listBot/
