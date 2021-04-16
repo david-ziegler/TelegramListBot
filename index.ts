9383affac7e229b9020d2a81468e5aeab5d95201
David Ziegler <david-ziegler@posteo.de>
2021-04-16 19:39:10 +0200

    new / don't show two console-logs saying the web-server started

--- index.ts
+++ index.ts
@@ -6,12 +6,10 @@ import { ENV } from './src/adapters/environment-variables';
 const app = express();
 const PORT = parseInt(ENV.PORT);
 app.listen(PORT, ENV.HOST, () => {
-  console.log(`Web server started at http://${ENV.HOST}:${PORT}`);
+  console.log(`Web server started at http://${ENV.HOST}:${PORT}. Version: ${packageInfo.version}. Production mode: ${ENV.IS_PRODUCTION_MODE}`);
 });
 
 app.post(`/${ENV.BOT_TOKEN}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
-
-console.log(`Bot server started. Version: ${packageInfo.version}. Production mode: ${ENV.IS_PRODUCTION_MODE}`);
