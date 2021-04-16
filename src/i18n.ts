b788ba877cdac54f21b6c32fea96fda6a2c3321b
David Ziegler <david-ziegler@posteo.de>
2021-04-16 18:52:47 +0200

    new / don't show help in every message

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,3 +1,3 @@
 export const i18n = {
-  message_text: '*Eintrag hinzufügen:*  `/list blabla`  schreiben\n*Eintrag abhaken:*  Auf Eintrag tippen\n*Liste anzeigen:*  `/list`  schreiben',
+  message_text: '*Einkaufsliste*',
 };
